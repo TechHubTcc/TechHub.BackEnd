2de044c62f2a3e5bfed67ccbb3019050d92ef985
SFullBox <fernando.limadsilva000@gmail.com>
2025-03-03 23:43:28 -0300

    Atualização na parte de modificar o telefone

--- src/main/java/com/api/reserva/service/UsuarioService.java
+++ src/main/java/com/api/reserva/service/UsuarioService.java
@@ -26,84 +26,85 @@ public class UsuarioService {
 
     //Listar todas as pessoas
     public List<UsuarioDTO> listar() {
-
         List<Usuario> usuarios = repository.findAll();
         return usuarios.stream().map(UsuarioDTO::new).toList();
-
     }
-        //Cadastrar pessoa=-
-        public UsuarioDTO salvar (UsuarioDTO user){
-            //validação de duplicidade
-            if (repository.existsByEmailOrTelefone(user.getEmail(), user.getTelefone())) {
-                throw new UsuarioDuplicadoException();
-            }
-            Usuario usuario = new Usuario(user);
-            usuario.setStatus(UsuarioStatus.ATIVO);
-            usuario.setRole(UsuarioRole.ESTUDANTE);
-            return new UsuarioDTO(repository.save(usuario));
+
+    //Cadastrar pessoa
+    public UsuarioDTO salvar (UsuarioDTO user){
+        //Validação de duplicidade
+        if (repository.existsByEmailOrTelefone(user.getEmail(), user.getTelefone())) {
+            throw new UsuarioDuplicadoException();
         }
+        Usuario usuario = new Usuario(user);
+        usuario.setStatus(UsuarioStatus.ATIVO);
+        usuario.setRole(UsuarioRole.ESTUDANTE);
+        return new UsuarioDTO(repository.save(usuario));
+    }
 
-        //Atualizar pessoa
-        public UsuarioDTO atualizartudo (UsuarioDTO user, Long id){
-            //verificando existencia no banco
-            Usuario usuario = repository.findById(id).orElseThrow(() -> new SemResultadosException("atualização."));
+    //Atualizar pessoa completamente
+    public UsuarioDTO atualizartudo (UsuarioDTO user, Long id){
+        //Verificando existência no banco
+        Usuario usuario = repository.findById(id).orElseThrow(() -> new SemResultadosException("atualização."));
 
-            if (repository.existsByEmailOrTelefoneAndIdNot(user.getEmail(), user.getTelefone(), id)) {
-                throw new UsuarioDuplicadoException();
-            }
+        if (repository.existsByEmailOrTelefoneAndIdNot(user.getEmail(), user.getTelefone(), id)) {
+            throw new UsuarioDuplicadoException();
+        }
+        usuario.setNome(user.getNome());
+        usuario.setEmail(user.getEmail());
+        usuario.setSenha(user.getSenha());
+        usuario.setTelefone(user.getTelefone());
+        usuario.setGenero(user.getGenero());
+        usuario.setStatus(user.getStatus());
+        usuario.setRole(user.getRole());
+        return new UsuarioDTO(repository.save(usuario));
+    }
+
+    //Atualizar parcialmente
+    public UsuarioDTO atualizar (UsuarioDTO user, Long id){
+        Usuario usuario = repository.findById(id).orElseThrow(() -> new SemResultadosException("atualização."));
+
+        if (!user.getNome().equals(usuario.getNome())) {
             usuario.setNome(user.getNome());
+        }
+
+        if (!user.getEmail().equals(usuario.getEmail())) {
             usuario.setEmail(user.getEmail());
+        }
+
+        if (!user.getSenha().equals(usuario.getSenha())) {
             usuario.setSenha(user.getSenha());
-            usuario.setTelefone(user.getTelefone());
+        }
+
+        // código feito pelo fernando, altas chances de ter que alterar, mas acho que nem vai precisarkkkkkkkk
+        String telefone = user.getTelefone().replaceAll("[\\s\\-()]", "");
+        if(telefone.length() == 11){
+            if (!telefone.equals(usuario.getTelefone())) {
+                usuario.setTelefone(telefone);
+            }
+
+        }
+
+        if (!user.getGenero().equals(usuario.getGenero())) {
             usuario.setGenero(user.getGenero());
+        }
+
+        if (!user.getStatus().equals(usuario.getStatus())) {
             usuario.setStatus(user.getStatus());
+        }
+
+        if (!user.getRole().equals(usuario.getRole())) {
             usuario.setRole(user.getRole());
-            return new UsuarioDTO(repository.save(usuario));
         }
 
-        public UsuarioDTO atualizar (UsuarioDTO user, Long id){
-            Usuario usuario = repository.findById(id).orElseThrow(() -> new SemResultadosException("atualização."));
-
-            if (!user.getNome().equals(usuario.getNome())) {
-                usuario.setNome(user.getNome());
-            }
-
-            if (!user.getEmail().equals(usuario.getEmail())) {
-                usuario.setEmail(user.getEmail());
-            }
-
-            if (!user.getSenha().equals(usuario.getSenha())) {
-                usuario.setSenha(user.getSenha());
-            }
-
-            if (user.getTelefone().length() == 11) {
-                if (!user.getTelefone().equals(usuario.getTelefone())) {
-                    String telefone = user.getTelefone();
-                    telefone.replace(" ", "");
-                    telefone.replace("-", "");
-                    usuario.setTelefone(user.getTelefone());
-                }
-            }
-
-            if (!user.getGenero().equals(usuario.getGenero())) {
-                usuario.setGenero(user.getGenero());
-            }
-
-            if (!user.getStatus().equals(usuario.getStatus())) {
-                usuario.setStatus(user.getStatus());
-            }
-
-            if (!user.getRole().equals(usuario.getRole())) {
-                usuario.setRole(user.getRole());
-            }
-            return new UsuarioDTO(repository.save(usuario));
-        }
-
-        //Deletar pessoa
-        public void deletar (Long id){
-            if (!repository.existsById(id)) {
-                throw new SemResultadosException();
-            }
-            repository.deleteById(id);
-        }
+        return new UsuarioDTO(repository.save(usuario));
     }
+
+    //Deletar pessoa
+    public void deletar (Long id){
+        if (!repository.existsById(id)) {
+            throw new SemResultadosException();
+        }
+        repository.deleteById(id);
+    }
+}
